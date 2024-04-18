29a44ad357e334c25c62c5132663fa5ab8c3785a
“Matej <matej@supernova.io>
2024-04-17 20:03:00 -0500

    Added remaining screen

--- Phase 3/main.js
+++ Phase 3/main.js
@@ -1,66 +1,52 @@
-const editor = document.querySelector(".editor");
-const tokens = document.querySelector(".tokens");
+var stateAuthorizing = "loading";
+var stateLoading = "resting";
+var stateImporting = "resting";
 
-const radioInputs = document.querySelectorAll('input[name="startScreen"]');
-
-let startScreen = 2;
-
-radioInputs.forEach(function (input) {
-  input.addEventListener("change", function () {
-    if (this.checked) {
-      startScreen = this.value;
-      updateStartScreen();
-    }
-  });
-});
-
-function updateStartScreen() {
-  switch (startScreen) {
-    case "1":
-      {
-        editor.classList.add("active");
-        tokens.classList.remove("active");
-        updateClassNameToEditor();
-      }
-      break;
-    case "2":
-      {
-        editor.classList.remove("active");
-        tokens.classList.add("active");
-        updateClassNameToTokens();
-      }
-      break;
+function updateStateAuthorizing(value) {
+  stateAuthorizing = value;
+  switch (stateAuthorizing) {
+    case "resting": updateState('authorizing-figma', 'resting', 'Authorizing Figma'); break;
+    case "loading": updateState('authorizing-figma', 'loading', 'Authorizing Figma'); break;
+    case "success": updateState('authorizing-figma', 'success', 'Authorized Figma'); break;
+    case "error": updateState('authorizing-figma', 'error', 'Unable to authorize'); break;
   }
+};
+
+function updateStateLoading(value) {
+  stateLoading = value;
+  switch (stateLoading) {
+    case "resting": updateState('loading-styles', 'resting', 'Loading styles from Figma'); break;
+    case "loading": updateState('loading-styles', 'loading', 'Loading styles from Figma'); break;
+    case "success": updateState('loading-styles', 'success', 'Loaded styles from Figma'); break;
+    case "error": updateState('loading-styles', 'error', 'Unable to load Figma styles'); break;
+  }
+};
+
+function updateStateImporting(value) {
+  stateImporting = value;
+  switch (stateImporting) {
+    case "resting": updateState('importing-styles', 'resting', 'Importing styles as tokesn'); break;
+    case "loading": updateState('importing-styles', 'loading', 'Importing styles as tokens'); break;
+    case "success": updateState('importing-styles', 'success', 'Imported styles as tokens'); break;
+    case "error": updateState('importing-styles', 'error', 'Unable to import Figma styles'); break;
+  }
+};
+
+function updateState(elementId, state, label) {
+
+  var destination = document.getElementById(elementId);
+  
+  var allIcons = destination.querySelectorAll('.state-icon-resting, .state-icon-loading, .state-icon-success, .state-icon-error');
+  allIcons.forEach(item => item.style.display = 'none');
+
+  destination.querySelector('.state-icon-' + state).style.display = "block";
+  destination.querySelector('.state-label').innerHTML = label;
+
+  (state == 'resting') ? destination.querySelector('.state-label').style.color = "#7F8694" : destination.querySelector('.state-label').style.color = "#14171F";
+
+  (state == 'loading') ? destination.querySelector('.connect-line').style.display = "block" : destination.querySelector('.connect-line').style.display = "none";
 }
 
-function updateClassNameToEditor() {
-  // Get all elements
-  var elements = document.querySelectorAll('[class*=screen-tokens]');
-
-  // Iterate through the elements and update the class name
-  elements.forEach(function(element) {
-    var currentClassName = element.className;
-
-    // Replace "screen-tokens" with "screen-editor"
-    var newClassName = currentClassName.replace(/screen-tokens/g, "screen-editor");
-
-    // Update the class name of the element
-    element.className = newClassName;
-  });
-}
-
-function updateClassNameToTokens() {
-  // Get all elements
-  var elements = document.querySelectorAll('[class*=screen-editor]');
-
-  // Iterate through the elements and update the class name
-  elements.forEach(function(element) {
-    var currentClassName = element.className;
-
-    // Replace "screen-tokens" with "screen-editor"
-    var newClassName = currentClassName.replace(/screen-editor/g, "screen-tokens");
-
-    // Update the class name of the element
-    element.className = newClassName;
-  });
-}
+updateStateAuthorizing(stateAuthorizing);
+updateStateLoading(stateLoading);
+updateStateImporting(stateImporting);
